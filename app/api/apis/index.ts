7a9baef0f65209705c77a0c10a53122b03efdb2e
RamiroGhilino <ghilinoramiro@gmail.com>
2023-11-08 18:01:33 -0300

    Added API client

---
+++ app/api/apis/index.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AcademicUnitsApi';
+export * from './AcademicYearsApi';
+export * from './AreasApi';
+export * from './CareerXAreaApi';
+export * from './CareerXUserApi';
+export * from './CareersApi';
+export * from './PostulationXAreaApi';
+export * from './PostulationsApi';
+export * from './RolesApi';
+export * from './SchemaApi';
+export * from './SearchsApi';
+export * from './TagsApi';
+export * from './TokenApi';
+export * from './TutorUserReviewApi';
+export * from './TutorUserSchedulesApi';
+export * from './TutorshipInstancesApi';
+export * from './TutorshipReportsApi';
+export * from './UserXAreaApi';
+export * from './UserXRoleApi';
+export * from './UserXTutorshipInstanceXRoleApi';
+export * from './UsersApi';
