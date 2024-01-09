064c6c9386bfc2c5b9cbb8541027452970ce0fde
ju4ncito <villarrealjm2@gmail.com>
2024-01-09 16:48:39 -0300

    reset

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh'
-      }
-    ]
-  }
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
